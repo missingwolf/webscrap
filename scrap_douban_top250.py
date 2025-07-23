1a4445a64d5a590e4c8175e09c67930c38ce26c8
missingwolf <243716752@qq.com>
2025-07-24 01:25:46 +0800

    Optimize the printing results

--- scrap_douban_top250.py
+++ scrap_douban_top250.py
@@ -6,7 +6,7 @@ import time
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
 }
-query = input("你需要查询哪年之后的电影：")
+query_year = input("你需要查询哪年之后的电影：")
 count = 0
 for start_num in range(0, 250, 25):
     response = requests.get(f"https://movie.douban.com/top250?start={start_num}", headers=headers)
@@ -22,12 +22,12 @@ for start_num in range(0, 250, 25):
             year_match = re.search(r'(\d{4})', info_text)
             if year_match:
                 year = year_match.group(1)
-                if int(year) > int(query):
+                if int(year) >= int(query_year):
                     count += 1
-                    print(f"{title}，{year}")
+                    print(f"{title}，{query_year}")
             else:
                 print(f"{title}，未找到年份")
         else:
             print(f"{title}，未找到信息")
     time.sleep(2)
-print(f"总共{count}部")
+print(f"{query_year}年之后的电影，总共{count}部。")
